4750fffac3ccc50ec464e082c1c4ef980e863cc7
saera1988 <saera1988@hotmail.com>
2019-12-17 18:00:42 +0300

    changes made in temp branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import "./App.scss";
-import { Route } from "react-router-dom";
-import Home from "./Home/Home";
-import SideNav from "./header/SideNav";
-// import NavbarPage from "./header/NavbarPage";
-import Footer from "./Home/Footer";
-import AuthenticatedRoute from "./auth/components/AuthenticatedRoute";
-import Header from "./header/Header";
-import SignUp from "./auth/components/SignUp";
-import SignIn from "./auth/components/SignIn";
-import SignOut from "./auth/components/SignOut";
-import ChangePassword from "./auth/components/ChangePassword";
-import AlertDismissible from "./auth/components/AlertDismissible";
-import PatientDash from "./dashboard/Patient/PatientDash";
-import NewRequest from "./dashboard/Patient/NewRequest";
-import AssistantDash from "./dashboard/Assistant/AssistantDash";
-import Team from "./Home/Team";
-=======
 import React, { Component } from 'react'
 import './App.scss'
 import { Route } from 'react-router-dom'
@@ -35,28 +15,20 @@ import MyRequests from "./dashboard/Patient/myrequests/MyRequests"
 import NewRequest from "./dashboard/Patient/NewRequest"
 import AssistantDash from "./dashboard/Assistant/AssistantDash"
 import Team from "./Home/Team"
->>>>>>> af8d55fd32c155c10137792d66d3f61feb3b4eb6
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
       user: null,
-<<<<<<< HEAD
-=======
       assistantId: null,
->>>>>>> af8d55fd32c155c10137792d66d3f61feb3b4eb6
       userObj: {},
       alerts: []
     };
   }
 
   setUser = (user, userObj) => this.setState({ user, userObj });
-<<<<<<< HEAD
-
-=======
   setAssistantId = assistantId => this.setState({ assistantId });
->>>>>>> af8d55fd32c155c10137792d66d3f61feb3b4eb6
   clearUser = () => this.setState({ user: null });
 
   alert = (message, type) => {
@@ -64,11 +36,7 @@ class App extends Component {
   };
 
   render() {
-<<<<<<< HEAD
-    const { alerts, user } = this.state;
-=======
     const { alerts, user, userObj } = this.state;
->>>>>>> af8d55fd32c155c10137792d66d3f61feb3b4eb6
 
     return (
       <React.Fragment>
@@ -85,8 +53,6 @@ class App extends Component {
             path="/sign-up"
             render={() => <SignUp alert={this.alert} setUser={this.setUser} />}
           />
-<<<<<<< HEAD
-=======
 
           <Route
             path="/sign-in"
@@ -119,43 +85,10 @@ class App extends Component {
             exact path={`/Patient/:${userObj.token}/myrequests`}
             render={() => <MyRequests user={userObj} />}
           />
->>>>>>> af8d55fd32c155c10137792d66d3f61feb3b4eb6
           <Route path="/team" component={Team} />
           <Route path="/SideNav" component={SideNav} />
           {/* <Route path="/NavbarPage" component={NavbarPage} /> */}
           <Route exact path="/" component={Home} />
-<<<<<<< HEAD
-          <Route
-            path="/sign-in"
-            render={() => <SignIn alert={this.alert} setUser={this.setUser} />}
-          />
-          <AuthenticatedRoute
-            user={user}
-            path="/sign-out"
-            render={() => (
-              <SignOut
-                alert={this.alert}
-                clearUser={this.clearUser}
-                user={user}
-              />
-            )}
-          />
-          <AuthenticatedRoute
-            user={user}
-            path="/change-password"
-            render={() => <ChangePassword alert={this.alert} user={user} />}
-          />
-
-          <AuthenticatedRoute
-            user={user}
-            path="/patient"
-            render={() => <PatientDash />}
-          />
-          <AuthenticatedRoute
-            user={user}
-            path="/assistant"
-            render={() => <AssistantDash />}
-=======
        
      
     
@@ -169,7 +102,6 @@ class App extends Component {
             user={user}
             exact path="/assistant"
             render={() => <AssistantDash user={userObj} />}
->>>>>>> af8d55fd32c155c10137792d66d3f61feb3b4eb6
           />
         </main>
         <Footer />
