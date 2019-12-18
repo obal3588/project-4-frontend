7f88567cdfac02a314a8966d68b2875e81b50f26
obal3588 <obal3588@colorado.edu>
2019-12-18 18:12:01 +0300

    dd

--- src/auth/components/SignUp.js
+++ src/auth/components/SignUp.js
@@ -30,18 +30,18 @@ class SignUp extends Component {
   onSignUp = event => {
     event.preventDefault();
 
-    const { alert, history, setUser } = this.props;
+    const { history, setUser } = this.props;
 
     signUp(this.state)
       .then(() => signIn(this.state))
       .then(res => setUser(res.data.user.name)
       )
-      .then(() => alert(messages.signUpSuccess, "success"))
+
       .then(() => history.push("/"))
       .catch(error => {
         console.error(error);
         this.setState({ email: "", password: "", passwordConfirmation: "" });
-        alert(messages.signUpFailure, "danger");
+   
       });
   };
 
